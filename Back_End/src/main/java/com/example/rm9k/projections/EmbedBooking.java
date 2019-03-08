15108a50118bf3d16f03fa2512ba6ec4e1237c48
klkirstyleeskl <k.lees@btinternet.com>
2019-03-08 16:13:56 +0000

    adds customer id to embed

--- Back_End/src/main/java/com/example/rm9k/projections/EmbedBooking.java
+++ Back_End/src/main/java/com/example/rm9k/projections/EmbedBooking.java
@@ -7,6 +7,7 @@ import org.springframework.data.rest.core.config.Projection;
 
 @Projection(name = "embedBooking", types = Customer.class)
 public interface EmbedBooking {
+    Long getId();
     String getCustomerName();
     String getCustomerNumber();
 }
