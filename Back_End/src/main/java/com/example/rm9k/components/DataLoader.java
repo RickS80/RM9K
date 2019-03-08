80e3fdb0cb092f39540258503602cf93666f9418
Rick Symington <rick.symington@gmail.com>
2019-03-08 14:18:40 +0000

    adds extra data to db

--- Back_End/src/main/java/com/example/rm9k/components/DataLoader.java
+++ Back_End/src/main/java/com/example/rm9k/components/DataLoader.java
@@ -35,6 +35,28 @@ public class DataLoader implements ApplicationRunner {
         restaurantTableRepository.save(restaurantTable1);
         Booking booking1 = new Booking(customer1, restaurantTable1);
         bookingRepository.save(booking1);
+
+        Customer customer2 = new Customer("Rick", "1231234");
+        customerRepository.save(customer2);
+        RestaurantTable restaurantTable2 = new RestaurantTable(1, 5);
+        restaurantTableRepository.save(restaurantTable2);
+        Booking booking2 = new Booking(customer2, restaurantTable2);
+        bookingRepository.save(booking2);
+
+        Customer customer3 = new Customer("John", "1013994");
+        customerRepository.save(customer3);
+        RestaurantTable restaurantTable3 = new RestaurantTable(18, 2);
+        restaurantTableRepository.save(restaurantTable3);
+        Booking booking3 = new Booking(customer3, restaurantTable3);
+        bookingRepository.save(booking3);
+
+        Booking booking4 = new Booking(customer2, restaurantTable1);
+        bookingRepository.save(booking4);
+
+        Booking booking5 = new Booking(customer1, restaurantTable3);
+        bookingRepository.save(booking5);
+
+
         }
 
 
