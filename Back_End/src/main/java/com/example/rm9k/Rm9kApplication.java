fe1533a2d52b0942e75a596cec496b3b4db080d5
Rick Symington <rick.symington@gmail.com>
2019-03-08 10:30:48 +0000

    Add back_end folder

---
+++ Back_End/src/main/java/com/example/rm9k/Rm9kApplication.java
@@ -0,0 +1,13 @@
+package com.example.rm9k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rm9kApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rm9kApplication.class, args);
+	}
+
+}
---
+++ Back_End/src/test/java/com/example/rm9k/Rm9kApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.rm9k;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Rm9kApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
