84f5c45c3667e7fdaa80c829dfd27f8b487efc66
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-03-09 15:22:15 +0000

    add placeholder content and CSS to customerModalContent

--- Front_End/src/components/CustomerModal/CustomerModalContent.js
+++ Front_End/src/components/CustomerModal/CustomerModalContent.js
@@ -1,3 +1,19 @@
+import 'CustomerModalContent.css';
+
 const CustomerModalContent = () => {
-  return <div>Modal content to go here</div>;
+  return (
+    <aside className="c-modal-cover"> // darkened backdrop
+      <div className="c-modal"> // actual modal area
+        <button className="c-modal__close">
+          <span className="u-hide-visually">Close</span>
+          <svg className="c-modal__close-icon" viewBox="0 0 40 40"><path d="M 10,10 L 30,30 M 30,10 L 10,30"></path></svg>
+        </button>
+        <div className="c-modal__body">
+          CONTENT WILL GO HERE
+        </div>
+      </div>
+    </aside>
+    )
 };
+
+export default CustomerModalContent;
