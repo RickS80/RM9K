a0a4cab33fccc86b115a29375f1ff1cc9279aa87
Davide Venditti <davide_335@msn.com>
2019-03-09 10:03:17 +0000

    header from yesterday sotred

--- Front_End/src/components/Header.js
+++ Front_End/src/components/Header.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Header extends Component{
-    render(){
-        return(
+const Header = (props) => {
+       return(
             <div className="header">
             <a>Home</a>
             <a>New booking</a>
@@ -10,6 +9,5 @@ class Header extends Component{
             </div>
         )
     }
-}
 
 export default Header;
