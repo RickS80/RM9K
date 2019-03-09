a9e2b6fc85c158c9ac37b38a781fc105681b1cc5
Davide Venditti <davide_335@msn.com>
2019-03-09 21:10:53 +0000

    tried different attempt on the tableView

--- Front_End/src/components/Table.js
+++ Front_End/src/components/Table.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Table = (props) => {
 
-  const bookingsData = props.bookingsData.map((booking) => {
+  const bookingsDataForList = props.bookingsData.map((booking) => {
      return <ul>
             <li>{"Name"} : {booking.customer.customerName}</li>
             <li>{"Number"} {booking.customer.customerNumber}</li>
@@ -10,10 +10,21 @@ const Table = (props) => {
             </ul>
   })
 
+  const bookingsDataForTable = props.bookingsData.map((booking) => {
+    console.log(booking)
+     return <td>{booking.customer.customerName}</td>
+  })
+
     const tables = [1,2,3,4,5,6,7,8,9,10]
     const tablesData = tables.map((table) =>{
-      return <th>Tab{table}</th>
+      return <th value={table} key={table}>Tab{table}</th>
     })
+    
+    const times = [12.00,12.30,13.00,13.30,14.00,14.30,15.00,15.30,16.00,16.30]
+    const timesData = times.map((time) =>{
+      return <tr><th>{time}</th></tr>
+    })
+
 
 
 
@@ -21,8 +32,10 @@ const Table = (props) => {
     return(
 
       // <div>
-      //   {bookingsData}
+      //   {bookingsDataForList}
       // </div>
+
+
 
       <div>
       <table>
@@ -34,29 +47,16 @@ const Table = (props) => {
           </thead>
             <tbody>
           <tr>
-            <td>12.00</td>
-            <td>Mark Davidson</td>
-            <td>4</td>
-            <td>35</td>
-            <td>07928989892</td>
+            <td >12.00</td>
           </tr>
           <tr>
             <td>12.30</td>
-            <td>Albert Smith</td>
-            <td>2</td>
-            <td>19</td>
-            <td>Albert@sas.com</td>
           </tr>
           <tr>
             <td>13.00</td>
-            <td>Larry</td>
-            <td>2</td>
-            <td>101</td>
-            <td>No contacts</td>
           </tr>
           <tr>
             <td>13.30</td>
-           
           </tr>
           <tr>
             <td>14.00</td>  
