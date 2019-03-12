509ca5ffda058e34f0107708a15d03c22ddac1f8
klkirstyleeskl <k.lees@btinternet.com>
2019-03-12 14:46:47 +0000

    attempts PUT

--- Back_End/src/main/java/com/example/rm9k/controllers/BookingController.java
+++ Back_End/src/main/java/com/example/rm9k/controllers/BookingController.java
@@ -3,12 +3,12 @@ package com.example.rm9k.controllers;
 import com.example.rm9k.models.Booking;
 import com.example.rm9k.models.RestaurantTable;
 import com.example.rm9k.repositories.BookingRepository;
+import com.example.rm9k.repositories.RestaurantTableRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.*;
 
+import java.awt.print.Book;
 import java.util.List;
 
 @RestController
@@ -23,10 +23,22 @@ public class BookingController {
         return bookingRepository.findBookingsByDate(date);
     }
 //
-    @GetMapping("/date/{date}/startTime/{startTime}")
+    @GetMapping("/date/{date}/startTime/{startTime}/restaurantTable/{restaurantTable}")
     public List<Booking> findBookingByDateAndStartTimeAndRestaurantTable
-        (@PathVariable String date, String startTime, RestaurantTable restaurantTable){
+        (@PathVariable String date, @PathVariable String startTime, @PathVariable RestaurantTable restaurantTable){
+        //need to be able to pull a restaurantTable object out of database, using the id number given above.
+
+
+
+//        RestaurantTable restaurantTable = RestaurantTableRepository.getTable ?????????
+
+        //then use the table object to run the find query
         return bookingRepository.findBookingByDateAndStartTimeAndRestaurantTable(date, startTime, restaurantTable);
+    }
+
+    @PostMapping
+    public Booking addBooking(@RequestBody Booking booking){
+        return null;
     }
 
 
