2c77a6004a57ed0c39c1326ecc426f3991491627
klkirstyleeskl <k.lees@btinternet.com>
2019-03-13 14:41:20 +0000

    adds colour change

--- Front_End/src/components/BookingRow.js
+++ Front_End/src/components/BookingRow.js
@@ -1,34 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BookingRow = (props) => {
+class BookingRow extends Component{
 
-
-    const times =  ["12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","19:00","19:30","20:00"]
-
-    const data = []
-
-
-
-    for(var i=0; i < times.length; i++){
-        // for (var j=0; j< props.bookingsData.length; j++){
-            // if (times[i] === props.bookingsData[j].startTime){
-                const foundBooking = props.bookingsData.find((booking) => booking.startTime === times[i])
-                if (foundBooking) {
-                    let number = foundBooking.id
-                data.push(
-                    <td id={foundBooking.id} onClick={evt => {props.passUp(number)}} >{foundBooking.customer.customerName}|{foundBooking.covers}|</td>
-                )
-            }else{
-                data.push(
-                    <td class="showhide"></td>
-                )
-            }
+    constructor(props){
+      super(props);
+      this.state = {
+        colorClass: "showhide"
+      }
+      this.handleClick = this.handleClick.bind(this);
     }
 
+    handleClick(){
+      this.setState({
+        colorClass: "showhide2"
+      })
+    }
 
-    return(
-        data
-    )
+    render(){
+      const times =  ["12:00","12:30","13:00","13:30","14:00",
+      "14:30","15:00","15:30","16:00","16:30","17:00","17:30",
+      "18:00","18:30","19:00","19:30","20:00"]
+      const data = []
+      for(var i=0; i < times.length; i++){
+          // for (var j=0; j< props.bookingsData.length; j++){
+              // if (times[i] === props.bookingsData[j].startTime){
+                  const foundBooking = this.props.bookingsData.find((booking) => booking.startTime === times[i])
+                  if (foundBooking) {
+                      let number = foundBooking.id
+                  data.push(
+                      <td id={foundBooking.id} class={this.state.colorClass} onClick={evt => {this.props.passUp(number)}, this.handleClick} >{foundBooking.customer.customerName}|{foundBooking.covers}|</td>
+                  )
+              }else{
+                  data.push(
+                      <td></td>
+                  )
+              }
+      }
+      return data;
+  }
 
 
 
